1f570797eafbb71a933e5a20a1b48875312f34a1
ericcheungwork <ericcheungwork@gmail.com>
2017-06-04 17:10:37 +0800

    Initial Commit

---
+++ LalamoveDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LalamoveDemo
+//
+//  Created by oOEric on 4/6/2017.
+//  Copyright © 2017 Eric Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
